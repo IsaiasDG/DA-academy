0438337016afc065223f7595ff87713b1f283bac
Isaias Garcia <gb_igarcia@idcontractors.net>
2020-01-14 19:04:12 -0600

    [DAA][Python] added empty class

---
+++ matrix_test.py
@@ -0,0 +1,24 @@
+import Matrix
+
+from unittest import TestCase
+
+
+class MatrixTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_build(self):
+        pass
+
+    def test_build_fail_elements_incogruents(self):
+        pass
+
+    def test_add(self):
+        pass
+
+    def test_scalar_multiplication(self):
+        pass
+
+    def test_multiplication(self):
+        pass
---
+++ mod_1/matrix.py
@@ -0,0 +1,10 @@
+class Matrix(list):
+
+    def __init__(self):
+        pass
+
+    def __mult__(self, multiplier: Matrix) -> Matix:
+        return Matrix()
+
+    def __str__(self) -> str:
+        return ""
